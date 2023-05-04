e83cc4752ac0b13e197722d1168842b24ad934ce
Gabriel Monroy <gmonroy@cisco.com>
2023-05-04 08:49:49 -0400

    burst optimization + summarize real_ip

--- Access Log Analyzer/README.md
+++ Access Log Analyzer/README.md
@@ -1,88 +1,119 @@
+- [accLogAnalyzer](#accloganalyzer)
+  - [How to use](#how-to-use)
+  - [Outputs](#outputs)
+  - [acclogAnalysis Summary example](#accloganalysis-summary-example)
+  - [Burst Analysis Example](#burst-analysis-example)
+
 # accLogAnalyzer
 
 accLogAnalyzer, written to be run directly on an APIC.
 
-Scrapes all entries in the latest access.log and aggregates it into a summary output.
+Scrapes all entries in the latest access.log and aggregates it into a summary output + burst aggregation file.
 
 ## How to use
 
+```
+apic# /tmp/accLogAnalyzer.py -h
+usage: accLogAnalyzer.py [-h] [-f [FILENAME]] [-o [OUTPUT_LOCATION]]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f [FILENAME], --file [FILENAME]
+                        Fullpath to access.log file to analyze
+  -o [OUTPUT_LOCATION], --output [OUTPUT_LOCATION]
+                        Directory to write output files
+```
+
 1. Copy the script onto the APIC `/tmp`
 2. `chmod 755 /tmp/accLogAnalyzer.py`
-3. `/tmp/accLogAnalyzer.py` to run
+3. `/tmp/accLogAnalyzer.py` to run with default locations
 
-output is written to `/data/techsupport/acclogAnalysis_<datetime>.output`
+default locations are:
+
+- Look for the latest `access.log` at `/var/log/dme/log/` (default APIC location)
+- write output to `/data/techsupport/acclogAnalysis_<datetime>.output`
 
 ```
-apic1# /tmp/acclogtest.py       
-29-Apr-2022 (13:27:42.977702) Starting accLogAnalyzer
-29-Apr-2022 (13:27:42.978161) Found /var/log/dme/log/access.log, beginning run
-29-Apr-2022 (13:27:42.978205) Parsing START
-29-Apr-2022 (13:27:43.629108) Parsing FIN, begin analysis
-29-Apr-2022 (13:27:43.629199) Got 40790 lines to analyze, this could take a few minutes...
-29-Apr-2022 (13:36:16.712436) Run finished, Wrote analysis to /data/techsupport/acclogAnalysis_2022-04-29T13:27:42.output
+apic1# /tmp/accLogAnalyzer.py
+03-May-2023 (19:54:53.705990) Starting accLogAnalyzer
+03-May-2023 (19:54:53.706246) Found /var/log/dme/log/access.log, beginning run
+03-May-2023 (19:54:53.706360) Parsing START
+03-May-2023 (19:54:54.350068) Parsing FIN. Begin Summary.
+03-May-2023 (19:54:54.350193) Got 34180 lines to summarize
+03-May-2023 (19:55:28.059610) Run finished, Wrote Summary to /data/techsupport/acclogAnalysis_2023-05-03T19:54:53.output
+03-May-2023 (19:55:28.059854) Wrote Burst analysis to /data/techsupport/acclogBurstAnalysis_2023-05-03T19:54:53.output
 ```
 
-## Output Contents example
+## Outputs
+
+This script produces 2 output files:
+
+1. The summary file; `acclogAnalysis*`
+2. The burst analysis file: `acclogBurstAnalysis*`
+
+## acclogAnalysis Summary example
+
 ```
-apic1# cat /data/techsupport/acclogAnalysis_2022-04-29T13:27:42.output
-Access Log Time Analysis Summary: 
+apic1# cat /data/techsupport/acclogAnalysis_2023-05-03T19:54:53.output
+Access Log Time Analysis Summary:
 
-    Log Start Time: 27/Apr/2022:14:01:04
-    Log End Time: 29/Apr/2022:13:27:00
+    Log Start Time: 02/May/2023:20:17:13
+    Log End Time: 03/May/2023:19:54:47
 
-    Total # of Requests: 40790
+    Total # of Requests: 34180
 
-    Time Coverage: 2845.93333333 Minutes (170756.0 s)
-    Avg # of Reqs: 0.24 Queries per second
+    Time Coverage: 1417.5666666666666 Minutes (85054.0 s)
+    Avg # of Reqs: 0.4 Queries per second
 
-    Burst Summary: 267 15+ requests-per-second bursts found.
+    Burst Summary: 480 15+ requests-per-second bursts found.
 
-Remote Address Summary: 
+Remote Address Summary:
 
-    Remote addr '192.168.1.19' request count: 18239 (%44.71 of total reqs) 
-    Remote addr '192.168.2.149' request count: 5930 (%14.54 of total reqs) 
-    Remote addr '10.0.0.1' request count: 11364 (%27.86 of total reqs) 
-    Remote addr '127.0.0.1' request count: 5257 (%12.89 of total reqs) 
+    Remote addr '127.0.0.1' request count: 34180 (%100.0 of total reqs)
 
-User-Agent Summary: 
+Real IP Summary (Proxy-in-play):
 
-    User-agent 'Go-http-client/...' request count: 5223 (%12.8 of total reqs) 
-    User-agent 'python-requests...' request count: 2 (%0.0 of total reqs) 
-    User-agent 'Java/1.8.0_222...' request count: 11364 (%27.86 of total reqs) 
-    User-agent 'Mozilla/5.0 (Wi...' request count: 24201 (%59.33 of total reqs) 
+    Real IP '10.0.0.1' request count: 14305 (%41.85 of total reqs)
+    Real IP '127.0.0.1' request count: 10230 (%29.93 of total reqs)
+    Real IP 'None' request count: 2548 (%7.45 of total reqs)
+    Real IP '172.18.217.56' request count: 737 (%2.16 of total reqs)
+    Real IP '10.122.254.141' request count: 72 (%0.21 of total reqs)
+    Real IP '10.24.246.160' request count: 6288 (%18.4 of total reqs)
 
-Response Status Summary: 
+User-Agent Summary:
 
-    Response code '200' count: 34825 (%85.38 of total reqs) 
-    Response code '101' count: 1 (%0.0 of total reqs) 
-    Response code '403' count: 5962 (%14.62 of total reqs) 
-    Response code '400' count: 2 (%0.0 of total reqs) 
+    User-agent 'Python-urllib/3...' request count: 285 (%0.83 of total reqs)
+    User-agent 'python-requests...' request count: 5874 (%17.19 of total reqs)
+    User-agent 'Go-http-client/...' request count: 18749 (%54.85 of total reqs)
+    User-agent 'python-requests...' request count: 2876 (%8.41 of total reqs)
+    User-agent '-...' request count: 36 (%0.11 of total reqs)
+    User-agent 'go-resty/2.7.0 ...' request count: 72 (%0.21 of total reqs)
+    User-agent 'Mozilla/5.0 (Wi...' request count: 6288 (%18.4 of total reqs)
+
+Response Status Summary:
+
+    Response code '200' count: 34033 (%99.57 of total reqs)
+    Response code '400' count: 12 (%0.04 of total reqs)
+    Response code '403' count: 134 (%0.39 of total reqs)
+    Response code '304' count: 1 (%0.0 of total reqs)
 
 =============Summary End================
+```
 
-Burst Analysis - Checking for 15+ requests-per-second
+## Burst Analysis Example
 
-59 Request Burst found at 27/Apr/2022:14:01:08:
+```
+apic1# tail -n 40 /data/techsupport/acclogBurstAnalysis_2023-05-03T21:13:09.output
 
-59 Request Burst found at 27/Apr/2022:14:01:31:
+18 Request Burst found at 03/May/2023:21:06:04:
+     127.0.0.1 ::ffff 10.0.0.1 03/May/2023:21:06:04 GET /api/class/fvCEp.json?rsp-subtree-include=count HTTP/1.1 200 108 - python-requests/2.28.2
+     127.0.0.1 ::ffff 10.0.0.1 03/May/2023:21:06:04 GET /api/class/fvIp.json?rsp-subtree-include=count HTTP/1.1 200 108 - python-requests/2.28.2
+     127.0.0.1 ::ffff 10.0.0.1 03/May/2023:21:06:04 GET /api/class/fvBD.json?rsp-subtree-include=count HTTP/1.1 200 108 - python-requests/2.28.2
+     ...snip...
 
-59 Request Burst found at 27/Apr/2022:14:01:38:
-
-55 Request Burst found at 27/Apr/2022:14:02:01:
-
-57 Request Burst found at 27/Apr/2022:14:02:08:
-
-54 Request Burst found at 27/Apr/2022:14:02:31:
-
-58 Request Burst found at 27/Apr/2022:14:02:38:
-
-59 Request Burst found at 27/Apr/2022:14:03:01:
-
-59 Request Burst found at 27/Apr/2022:14:03:08:
-
-59 Request Burst found at 27/Apr/2022:14:03:31:
-
-57 Request Burst found at 27/Apr/2022:14:03:38:
-
-Burst Display Limit of 10 hit, bursts output stopped
+18 Request Burst found at 03/May/2023:21:11:04:
+     127.0.0.1 ::ffff 10.0.0.1 03/May/2023:21:11:04 GET /api/class/fvCEp.json?rsp-subtree-include=count HTTP/1.1 200 108 - python-requests/2.28.2
+     127.0.0.1 ::ffff 10.0.0.1 03/May/2023:21:11:04 GET /api/class/fvIp.json?rsp-subtree-include=count HTTP/1.1 200 108 - python-requests/2.28.2
+     127.0.0.1 ::ffff 10.0.0.1 03/May/2023:21:11:04 GET /api/class/fvBD.json?rsp-subtree-include=count HTTP/1.1 200 108 - python-requests/2.28.2
+     ...snip...
 ```
--- Access Log Analyzer/accLogAnalyzer.py
+++ Access Log Analyzer/accLogAnalyzer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
+from datetime import datetime
 import argparse
 import os
 import re
-from datetime import datetime
-import io
 
-ACCLOG_REGEX = r'^(?P<RemoteAddrV6>::ffff|):?(?P<RemoteAddr>\d+\.\d+\.\d+\.\d+) (?:|\(.+\) )- (.*) \[(?P<TimeLocal>\d+\/\w+\/\d{4}:\d+:\d+:\d+).+]\s?\"(?P<Request>.*)\" (?P<Status>\d{3}) (?P<BodyBytesSent>\d+) \"(?P<HttpReferrer>.+)\" \"(?P<HttpUserAgent>.+)\"'
+ACCLOG_REGEX = r"^(?P<RemoteAddrV6>::ffff|):?(?P<RemoteAddr>\d+\.\d+\.\d+\.\d+) \((?:-|(?P<Real_RemoteAddrV6>::ffff|):?(?P<Real_RemoteAddr>\d+\.\d+\.\d+\.\d+))\)(?:.*) \[(?P<TimeLocal>\d+\/\w+\/\d{4}:\d+:\d+:\d+).+]\s?\"(?P<Request>.*)\" (?P<Status>\d{3}) (?P<BodyBytesSent>\d+) \"(?P<HttpReferrer>.+)\" \"(?P<HttpUserAgent>.+)\""
 
 
 def mprint(message):
@@ -24,23 +23,38 @@ def parseLogs(file):
     return output
 
 
-def task(filename):
+def check_for_bursts(access_dict):
+    i = 0
+    while i < len(access_dict):
+        yield access_dict[i]
+        i += 1
+
+
+def main(filename, output_location):
     timenow = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
-    summary = ""
-    output = ""
     mprint("Parsing START")
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         access_dict = parseLogs(file)
+    mprint("Parsing FIN. Begin Summary.")
+    mprint("Got {num} lines to summarize".format(num=len(access_dict)))
 
-    mprint("Parsing FIN, begin analysis")
+    summary = ""
+    request_remote_addr_count = {}
+    request_real_ip_count = {}
+    request_user_agent_count = {}
+    response_status_count = {}
 
-    mprint("Got {num} lines to analyze, this could take a few minutes...".format(num=len(access_dict)))
+    burst_output = "Burst Analysis - Checking for 15+ requests-per-second\n"
+    timestamp_agg = []
+    burst_threshold = 15
+    entry2 = None
+    entry_gen = check_for_bursts(access_dict)
 
     ## Get First and Last entries for Timerange or log
     summary += "Access Log Time Analysis Summary: \n\n"
-    summary += "    Log Start Time: {start}\n".format(start=access_dict[0]['TimeLocal'])
-    summary += "    Log End Time: {end}\n\n".format(end=access_dict[-1]['TimeLocal'])
+    summary += "    Log Start Time: {start}\n".format(start=access_dict[0]["TimeLocal"])
+    summary += "    Log End Time: {end}\n\n".format(end=access_dict[-1]["TimeLocal"])
     summary += "    Total # of Requests: {req_len}\n\n".format(req_len=len(access_dict))
 
     log_start = datetime.strptime(access_dict[0]["TimeLocal"], "%d/%b/%Y:%H:%M:%S")
@@ -51,108 +65,164 @@ def task(filename):
     qry_per_second_avg = len(access_dict) / time_diff_seconds
 
     summary += "    Time Coverage: {time_diff_minutes} Minutes ({time_diff_seconds} s)\n".format(
-        time_diff_minutes=time_diff_minutes, time_diff_seconds=time_diff_seconds)
-    summary += "    Avg # of Reqs: {round_req} Queries per second\n\n".format(round_req=round(qry_per_second_avg, 2))
-
-    output += "\n=============Summary End================\n\n"
-    output += "Burst Analysis - Checking for 15+ requests-per-second\n"
-
-    burst_timestamps = []
-    request_remote_addr_count = {}
-    request_user_agent_count = {}
-    response_status_count = {}
-    burst_display_limit = 10
+        time_diff_minutes=time_diff_minutes, time_diff_seconds=time_diff_seconds
+    )
+    summary += "    Avg # of Reqs: {round_req} Queries per second\n\n".format(
+        round_req=round(qry_per_second_avg, 2)
+    )
 
     for entry in access_dict:
-        burst_temp = []
-
-        ## FOR REMOTE ADDR SUMMARY
+        ## REMOTE ADDR SUMMARY
         if entry["RemoteAddr"] not in request_remote_addr_count.keys():
             request_remote_addr_count[entry["RemoteAddr"]] = 1
         else:
             request_remote_addr_count[entry["RemoteAddr"]] += 1
 
-        ## FOR USER-AGENT SUMMARY
+        ## REAL_IP SUMMARY
+        if entry["Real_RemoteAddr"] not in request_real_ip_count.keys():
+            request_real_ip_count[entry["Real_RemoteAddr"]] = 1
+        else:
+            request_real_ip_count[entry["Real_RemoteAddr"]] += 1
+
+        ## USER-AGENT SUMMARY
         if entry["HttpUserAgent"] not in request_user_agent_count.keys():
             request_user_agent_count[entry["HttpUserAgent"]] = 1
         else:
             request_user_agent_count[entry["HttpUserAgent"]] += 1
 
-        ## FOR RESPONSE STATUS SUMMARY
+        ## RESPONSE STATUS SUMMARY
         if entry["Status"] not in response_status_count.keys():
             response_status_count[entry["Status"]] = 1
         else:
             response_status_count[entry["Status"]] += 1
 
-        ## FOR BURST ANALYSIS
-        for entry2 in access_dict:
-            if (entry["TimeLocal"] == entry2["TimeLocal"]) and (entry["TimeLocal"] not in burst_timestamps):
-                burst_temp.append(entry2)
+        ## BURST ANALYSIS
+        burst_temp = []
+        burst = False
+        analyzing = True
 
-        if (len(burst_temp) >= 15) and (entry["TimeLocal"] not in burst_timestamps):
-            if (len(burst_timestamps) <= burst_display_limit):
-                output += "\n{len_burst_temp} Request Burst found at {entry_time}:\n".format(
-                    len_burst_temp=len(burst_temp), entry_time=entry['TimeLocal'])
-                '''
-                for bentry in burst_temp: ### WIP
-                    output += f"    {' '.join(str(x) for x in bentry.values())}\n"
-                '''
-            burst_timestamps.append(entry["TimeLocal"])
+        if not entry2:
+            entry2 = next(entry_gen)
 
-    if len(burst_timestamps) > burst_display_limit:
-        output += "\nBurst Display Limit of {burst_display_limit} hit, bursts output stopped\n".format(
-            burst_display_limit=burst_display_limit)
+        if entry["TimeLocal"] not in [d["timestamp"] for d in timestamp_agg]:
+            while analyzing:
+                if entry["TimeLocal"] == entry2["TimeLocal"]:
+                    burst_temp.append(entry2)
+                    try:
+                        entry2 = next(entry_gen)
+                    except StopIteration:
+                        analyzing = False
+
+                else:
+                    analyzing = False
+
+            ## Display Burst output
+            tmp_burst_count = len(burst_temp)
+            if tmp_burst_count >= burst_threshold:
+                burst = True
+                burst_output += (
+                    "\n{burst_count} Request Burst found at {entry_time}:\n".format(
+                        burst_count=tmp_burst_count,
+                        entry_time=entry["TimeLocal"],
+                    )
+                )
+
+                for bentry in burst_temp:
+                    burst_str = " ".join(str(x) for x in bentry.values())
+                    burst_output += f"    " + burst_str + "\n"
+
+            ## Write the burst summary dict
+            timestamp_summary = {
+                "timestamp": entry["TimeLocal"],
+                "count": tmp_burst_count,
+                "burst": burst,
+            }
+            timestamp_agg.append(timestamp_summary)
 
     ## BURST ANALYSIS SUMMARY
-    summary += "    Burst Summary: {len_burst_timestamps} 15+ requests-per-second bursts found.\n\n".format(
-        len_burst_timestamps=len(burst_timestamps))
+    total_bursts = len([i for i in timestamp_agg if i["burst"] == True])
+    summary += "    Burst Summary: {total_bursts} 15+ requests-per-second bursts found.\n\n".format(
+        total_bursts=total_bursts
+    )
 
     ## REMOTE ADDR SUMMARY
     summary += "Remote Address Summary: \n\n"
     for remote_addr, count in request_remote_addr_count.items():
         avg = round((float(count) / len(access_dict)) * 100, 2)
         summary += "    Remote addr '{remote_addr}' request count: {count} (%{avg} of total reqs) \n".format(
-            remote_addr=remote_addr, count=count, avg=avg)
+            remote_addr=remote_addr, count=count, avg=avg
+        )
+
+    ## REAL_IP SUMMARY
+    summary += "\nReal IP Summary (Proxy-in-play): \n\n"
+    for remote_addr, count in request_real_ip_count.items():
+        avg = round((float(count) / len(access_dict)) * 100, 2)
+        summary += "    Real IP '{remote_addr}' request count: {count} (%{avg} of total reqs) \n".format(
+            remote_addr=remote_addr, count=count, avg=avg
+        )
 
     ## USER-AGENT SUMMARY
     summary += "\nUser-Agent Summary: \n\n"
     for user_agent, count in request_user_agent_count.items():
         avg = round((float(count) / len(access_dict)) * 100, 2)
         summary += "    User-agent '{user_agent}...' request count: {count} (%{avg} of total reqs) \n".format(
-            user_agent=user_agent[:15], count=count, avg=avg)
+            user_agent=user_agent[:15], count=count, avg=avg
+        )
 
     ## RESPONSE STATUS SUMMARY
     summary += "\nResponse Status Summary: \n\n"
     for code, count in response_status_count.items():
         avg = round((float(count) / len(access_dict)) * 100, 2)
-        summary += "    Response code '{code}' count: {count} (%{avg} of total reqs) \n".format(code=code, count=count,
-                                                                                                avg=avg)
+        summary += "    Response code '{code}' count: {count} (%{avg} of total reqs) \n".format(
+            code=code, count=count, avg=avg
+        )
 
-    output_file = '/data/techsupport/acclogAnalysis_' + timenow + '.output'
-    with open(output_file, 'w') as f:
+    summary += "\n=============Summary End================\n\n"
+
+    output_file = output_location + "/acclogAnalysis_" + timenow + ".output"
+    with open(output_file, "w") as f:
         f.write(summary)
-        f.write(output)
 
-    mprint("Run finished, Wrote analysis to " + output_file)
+    mprint("Run finished, Wrote Summary to " + output_file)
+
+    burst_output_file = output_location + "/acclogBurstAnalysis_" + timenow + ".output"
+    with open(burst_output_file, "w") as f:
+        f.write(burst_output)
+
+    mprint("Wrote Burst analysis to " + burst_output_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument(
-        '-f', '--file', dest='filename', default="/var/log/dme/log/access.log",
-        help='gunzipped acess.log file to run against', required=False, nargs='+'
+        "-f",
+        "--file",
+        dest="filename",
+        default="/var/log/dme/log/access.log",
+        help="Fullpath to access.log file to analyze",
+        required=False,
+        nargs="?",
     )
-    mprint("Starting accLogAnalyzer")
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output_location",
+        default="/data/techsupport",
+        help="Directory to write output files",
+        required=False,
+        nargs="?",
+    )
     args = parser.parse_args()
     try:
         file = args.filename
+        output_location = args.output_location
         if os.path.exists(file):
-            mprint("Found {file}, beginning run".format(file=file))
-            task(file)
+            mprint("Found {file}, beginning accLogAnalyzer".format(file=file))
+            main(file, output_location)
         else:
-            mprint('{file} not found or not a file'.format(file=file))
+            mprint("{file} not found or not a file".format(file=file))
     except KeyboardInterrupt as e:
-        mprint('Keyboard break hit')
+        mprint("Keyboard break hit")
